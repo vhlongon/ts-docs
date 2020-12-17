7c67bd96790386b54c0a36c4f7ab647f910d1cf4
Victor <vhlongon@gmail.com>
2020-12-17 15:23:25 +0100

    add comments for variables

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -29,3 +29,35 @@ let point: { x: number; y: number } = {
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+// whe to use annotations
+
+// * when a fucntion returns the 'any' type and we need to clarify the value
+const json = '{"x": 10 , "y": 20}';
+// JSON.parse will return any because it can't know all the possible returns values
+// so annotation here is a good ideia
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates); // -> {x: 10, y: 20}
+
+// * when we declare a variable on one line then initialize it later
+let words = ['red', 'green', 'blue'];
+let found: boolean; // or initialize directly so TS can infere like let found = false;
+for (let i = 0; i < words.length; i++) {
+  const element = words[i];
+  if (element === 'green') {
+    found = true;
+  }
+}
+
+// * when we want the variable to have a type that can't be inferred correclty
+let nums = [-10, -1, 12];
+let numberAboveZero: boolean | number = false; // it can be either a boolean or number
+
+for (let i = 0; i < nums.length; i++) {
+  const element = nums[i];
+  if (element > 0) {
+    numberAboveZero = element;
+  }
+}
+
+//bottom line is that for most cases we dont need to do that since ts will be able to infere the types by itself:
